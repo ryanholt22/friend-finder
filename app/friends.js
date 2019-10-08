a9a3abc2667922e929358ee058a2b6e7d6972747
Ryan Holt <ryanholt22@yahoo.com>
2019-10-08 17:50:40 -0500

    finished friends.js

---
+++ app/friends.js
@@ -0,0 +1,27 @@
+var friends = [
+	{
+		name: "Ryan",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "John",
+		photo: "https://vignette2.wikia.nocookie.net/itsalwayssunny/images/f/f3/Square-Dee-Cheesy.jpg",
+		scores: [1, 3, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Sarah",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/1/13/Dennis_%281%29.jpg",
+		scores: [1, 5, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Noah",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Joe",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		scores: [1, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
