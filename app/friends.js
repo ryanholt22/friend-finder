8bc01001f8a40389b8c404eac3dabda47d2d4550
Ryan Holt <ryanholt22@yahoo.com>
2019-10-08 17:54:32 -0500

    finished friends.js

--- app/friends.js
+++ app/friends.js
@@ -1,27 +1,27 @@
 var friends = [
 	{
 		name: "Ryan",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+		photo: "https://bd.gaadicdn.com/processedimages/kawasaki/kawasaki-ninja-h2/640X309/v_ninja-h2-r1543231598.jpg",
 		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
 	},
 	{
 		name: "John",
-		photo: "https://vignette2.wikia.nocookie.net/itsalwayssunny/images/f/f3/Square-Dee-Cheesy.jpg",
+		photo: "https://cdn.nautal.com/media/boats/638/x/638212.jpg",
 		scores: [1, 3, 4, 1, 2, 3, 3, 2, 3, 3]
 	},
 	{
 		name: "Sarah",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/1/13/Dennis_%281%29.jpg",
+		photo: "https://images.homedepot-static.com/productImages/612ae505-9daf-45c3-ac16-67f97dcb251d/svn/globalrose-flower-bouquets-prime-100-red-roses-64_1000.jpg",
 		scores: [1, 5, 5, 3, 3, 5, 1, 2, 3, 4]
 	},
 	{
 		name: "Noah",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg",
+		photo: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/12/31/10/lion-face.jpg?w968",
 		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
 	},
 	{
 		name: "Joe",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		photo: "https://thenypost.files.wordpress.com/2019/04/pet-sematary-cat-2a.jpg?quality=90&strip=all&w=978&h=652&crop=1",
 		scores: [1, 1, 4, 4, 2, 4, 2, 5, 1, 3]
 	}
 ];
