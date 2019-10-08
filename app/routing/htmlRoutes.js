714b6d7b53b0e9d33d3dee31efdca0a9ca573206
Ryan Holt <ryanholt22@yahoo.com>
2019-10-08 17:26:17 -0500

    finished survey html just need to link it.

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
-var PORT = process.env.PORT || 8080;
+
+
 
 app.get("/survery", function(req, res) {
     if (err) throw "error1";
--- app/server.js
+++ app/server.js
@@ -1,2 +1,8 @@
 var express = require("express");
 var app = express();
+var PORT = process.env.PORT || 8080;
+var path = require("path");
+
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+});
