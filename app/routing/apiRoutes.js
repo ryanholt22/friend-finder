cc83e2f82fd78f1a04be67c2fabaac71931e0088
Ryan Holt <ryanholt22@yahoo.com>
2019-10-08 22:50:42 -0500

    please work

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,15 +4,12 @@ var path = require("path");
 
 app.get("/", function(req,res) {
     if (err) throw err;
-    res.sendFile("../public/home.html");
-})
-app.get("/api/friends", function(req, res) {
-    if (err) throw (err);
+    res.sendFile(path.join(__dirname, "../public/home.html"));
 })
 
 module.exports = function(app) {
 	// if user enters survey in URL or presses survey button, serves the survey HTML file
-	app.get("/survey", function(req, res) {
+	app.get("/survey.html", function(req, res) {
         res.sendFile(path.join(__dirname, "/../public/survey.html"));
     })
 };
